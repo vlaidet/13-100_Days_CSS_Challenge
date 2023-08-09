144195d25dfb29feade08e3c78f84c20e5b61874
Valentin Laidet <dev-ngu@protonmail.com>
2023-08-09 13:24:04 +0200

    Challenge 13 terminé

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const images = document.querySelectorAll('.image__container');
-    images.forEach((el) => {
-        el.addEventListener('click', function() {
-            console.log("click", el)
-        });
-    })
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".image__container");
+  const closeButton = document.querySelector(
+    ".gallery__overlay .header .close"
+  );
+  const galleryOverlay = document.querySelector(".gallery__overlay");
+
+  images.forEach((el) => {
+    el.addEventListener("click", function () {
+      !galleryOverlay.classList.contains("active")
+        ? galleryOverlay.classList.add("active")
+        : galleryOverlay.classList.remove("active");
+    });
+  });
+
+  closeButton.addEventListener("click", function () {
+    galleryOverlay.classList.remove("active");
+  });
 });
